ac300063638fa44e55b6feaf1bc4f837bfd3ca0e
shanjin987654@163.com <shanjin987654@163.com>
2015-09-25 16:17:07 +0800

    初始导入

---
+++ src/com/git/test/GitDemo.java
@@ -0,0 +1,34 @@
+/**   
+* @Title: GitDemo.java 
+* @Package com.git.test 
+* @Description: TODO
+* @author 金山
+* @date 2015-9-25 下午4:13:18 
+* @version V1.0   
+*/
+package com.git.test;
+
+/** 
+ * @ClassName: GitDemo 
+ * @Description: TODO
+ * @author 金山
+ * @date 2015-9-25 下午4:13:18 
+ *  
+ */
+public class GitDemo {
+
+	/** 
+	 * @Title: main 
+	 * @Description: TODO
+	 * @param @param args
+	 * @return void
+	 * @throws 
+	 * @author JinShan
+	 * @createTime 2015-9-25下午4:13:19
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello Git!");
+	}
+
+}
