efa81ad080262586df1dd39f542cb03cfc9aa5b6
shanjin987654@163.com <shanjin987654@163.com>
2015-09-25 16:51:01 +0800

    update test1

--- src/com/git/test/GitDemo.java
+++ src/com/git/test/GitDemo.java
@@ -29,6 +29,7 @@ public class GitDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello Git!");
+		System.out.println("hello Git2!");
 	}
 
 }
